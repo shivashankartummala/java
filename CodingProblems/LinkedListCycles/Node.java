b4b034434c25c0b08a007bb17fbc569fdcd55f64
SIVA SANKAR TUMMALA <shivashankar.tummala@gmail.com>
2017-10-02 21:18:02 -0700

    Add files via upload

---
+++ CodingProblems/LinkedListCycles/Node.java
@@ -0,0 +1,12 @@
+package com.sstummala.codingproblems.LinkedList;
+
+
+public class Node {
+    int data;
+    Node next;
+    public Node(int data, Node next){
+    	this.data = data;
+    	this.next = next;
+    	
+    }
+}
