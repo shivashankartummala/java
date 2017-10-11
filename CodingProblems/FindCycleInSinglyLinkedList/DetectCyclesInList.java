f516bf993316b5da4f443e0c972e247faba0cf22
SIVA SANKAR TUMMALA <shivashankar.tummala@gmail.com>
2017-10-10 22:18:58 -0700

    Add files via upload

---
+++ CodingProblems/FindCycleInSinglyLinkedList/DetectCyclesInList.java
@@ -0,0 +1,38 @@
+package com.sstummala.codingproblems.linkedlistcycles;
+
+public class DetectCyclesInList {
+		
+	
+	public static boolean hasCycle(Node head) {
+	    Node slow = head;
+	    Node fast = head;
+	   
+	    if(head == null) return false;
+	    
+	    while(fast !=null && fast.next != null){
+	        fast = fast.next.next;
+	        slow = slow.next;    
+	        if(fast == slow) return true;
+	    }
+	    return false;
+	}
+	
+	public static void main(String Args[]){
+		
+		Node n1,n2,n3,n4;
+		n3 = new Node(9,null);
+		n2 = new Node (4, n3);
+		n1 = new Node(3,n2);
+		
+		n4 = new Node(7,n2);
+		n3.next=n4;
+		
+		Node head = new Node(2, n1);
+		if (hasCycle(head))
+		System.out.println("List contains Cycles");
+		
+		
+		
+	}
+
+}
---
+++ CodingProblems/FindCycleInSinglyLinkedList/Node.java
@@ -0,0 +1,12 @@
+package com.sstummala.codingproblems.linkedlistcycles;
+
+
+public class Node {
+    int data;
+    Node next;
+    public Node(int data, Node next){
+    	this.data = data;
+    	this.next = next;
+    	
+    }
+}
