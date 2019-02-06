c17dd40e6cbcd41fa47c7eda787b43f201bc1536
SIVA SANKAR TUMMALA <shivashankar.tummala@gmail.com>
2019-02-06 11:34:49 -0800

    Add files via upload

---
+++ CodingProblems/LowestCommonAncestor/BinaryTreeCheckLCA.java
@@ -0,0 +1,33 @@
+package com.sstummala.codingproblems.tree;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class BinaryTreeCheckLCA {
+	int[] data = {25,17,10,15,22,24,40,30,50};
+	
+	BST bst = new BST(data);
+	
+	BinaryTreeLowestCommonAncestor lcaTest = new BinaryTreeLowestCommonAncestor();
+	
+	@Test
+	public void checkLCA() {
+		TreeTraversal.traverseInOrder(bst.root);
+		Node n = lcaTest.lca(bst.root,15,24);
+		System.out.println("Lowest Common Ancestor for 15,24 is  " + n.value);
+		assertEquals(17,n.value);
+		n = lcaTest.lca(bst.root,15,30);
+		System.out.println("Lowest Common Ancestor for 15,30 is  " + n.value);
+		assertEquals(25,n.value);
+		n = lcaTest.lca(bst.root,22,24);
+		System.out.println("Lowest Common Ancestor for 22,24 is  " + n.value);
+		assertEquals(22,n.value);
+		n = lcaTest.lca(bst.root,50,30);
+		System.out.println("Lowest Common Ancestor for 50,30 is  " + n.value);
+		assertEquals(40,n.value);
+	}
+
+}
